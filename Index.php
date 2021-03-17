7e546c4cf502041bd7859b36d6de8a1de2f9489e
Somaiyajannat <somaiyajannat044@gmail.com>
2021-03-17 12:09:34 +0600

    Initial Commit

---
+++ Index.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web Template</title>
+<link rel="stylesheet" href="styles.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+       
+</head>
+<body>
+	
+	<div class="head">
+		<div class="container">
+			<div class="row">
+				<div class="col-two">
+					<div class="logo">
+						<img src ="icon/logo.png" alt="logo">
+					</div>
+				</div>	
+				<div class="col-ten">
+					<div class="top-menu"> 
+						
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">News</a></li>
+							<li><a href="#">Pages</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+						
+					</div>
+				</div>
+			</div>
+		</div>	
+    </div>
+	
+	<div class="section2">
+		<div class="container">
+			<div class="row clear-both">
+				<div class="col-four">
+					<div class="second-item1">
+						<h6>AWESOME</h6>
+						<h1>SERVICE SEO</h1>
+						<h1> TEMPLATE</h1>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+						</p>
+						<h4>Read More</h4>						
+						
+					</div>
+				</div>	
+				<div class="col-four">
+					<div class="second-item1">
+						<img src="icon/2nd/1.png" alt ="icon1">
+						<h3>Establish Goals</h3>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+						<h4>Read More</h4>
+					</div>	
+				</div>
+				<div class="col-four">
+					<div class="second-item1">
+						<img src="icon/2nd/3.png" alt ="icon1">
+						<h3>Establish Goals</h3>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+						<h4>Read More</h4>
+					</div>	
+				</div>
+			</div>			
+		
+		<div class="row clear-both">
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+		</div>	
+	</div>	
+</div>	
+<div class="web-solution">
+		<div class="container">
+			<div class="row">
+				<div class="col-three">
+					<div class ="web-image">
+						<img src="icon/about/plane.png" alt="web Image">
+					</div>
+				</div>
+				<div class="col-seven">
+					<div class="web-solution-data">
+						<h2>ALL-in-one Web Solution For</h2>
+						<h2>your business</h2>
+						<p>
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ma
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>
+	<div class="sevices">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="services-header">
+						<h1> See Our Top Notvh Sevices</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+						</p>
+					</div>	
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-three">
+					<div class="services-items">
+							<img src="images/service/1.png" alt="">
+							<h4><b>Google SEO</b></h4>
+							<p>
+								Lorem ipsum dolor sit amet imperdiet ante. 
+							</p>
+							<a href="#">Read More</a>
+					</div>
+				</div>	
+					<div class="col-three">
+						<div class="services-items">
+							<img src="images/service/2.png" alt="">
+							<h4><b>Google SEO</b></h4>
+							<p>
+							Lorem ipsum dolor sit amet imperdiet ante. 
+							</p>
+							<a href="#">Read More</a>
+						</div>
+					</div>	
+				<div class="col-three">
+					<div class="services-items">
+							<img src="images/service/3.png" alt="">
+							<h4><b>Google SEO</b></h4>
+							<p>
+							Lorem ipsum dolor sit amet imperdiet ante. 
+							</p>
+							<a href="#">Read More</a>
+					</div>
+				</div>	
+				<div class="col-three">
+					<div class="services-items">
+						<img src="images/service/4.png" alt="">
+						<h4><b>Google SEO</b></h4>
+						<p>
+						Lorem ipsum dolor sit amet imperdiet ante. 
+						</p>
+						<a href="#">Read More</a>
+					</div>
+				</div>	
+			</div>
+		</div>	
+	</div>
+
+
+		
+	<div class="our-work">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="our-work-header">
+						<h2> OUR WORK </h2>
+						<p> 
+							simply dummy text of the printing and typesetting industry,<br> Lorem Ipsum has been the industry's s
+						</p>
+					</div>
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>
+				</div>	
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>	
+				</div>
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>	
+				</div>
+			</div>			
+		
+		<div class="row">
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-twelve">
+				<div class="work-button">
+					<h3> VIEW CAR WORK</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
+	
+	<div class="our-testimony">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="testimony-item">
+						<h1> Our Testimony</h1>
+						<img src="icon/testimony/1.png" alt ="testimony icon">
+					</div>	
+				</div>
+			</div>	
+		</div>
+	</div>
+	
+	<div class="our-plans">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="plan-header">
+						<h1> Our Plans</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+						</p>
+					</div>	
+					
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-three">
+					<div class="plan-items">
+						
+					</div>
+				</div>
+				<div class="col-three">
+					<div class="plan-items">
+					</div>
+				</div>	
+				<div class="col-three">
+					<div class="plan-items">
+					</div>
+				</div>
+				<div class="col-three">
+					<div class="plan-items">
+					</div>
+				</div>
+			</div>	
+		</div>	
+	</div>
+	
+	<div class="our-news">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="news-header">
+						<h1> Our News</h1>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+						</p>
+					</div>	
+					
+				</div>
+			</div>
+			<div class="row">
+					<div class="news-item">
+						<div class="col-three">
+							<img src="images/g.jpg" alt ="icon1">
+							<p>
+								<b>
+								Lorem ipsum dolor sit amet, consectetur
+								<b>
+							</p>
+							<p>May 12,2019</p>
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+							</p>
+						</div>
+						<div class="col-three">
+							<img src="images/g.jpg" alt ="icon1">
+							<p>
+								<b>
+								Lorem ipsum dolor sit amet, consectetur
+								<b>
+							</p>
+							<p>May 12,2019</p>
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+							</p>
+						</div>
+						<div class="col-three">
+							<img src="images/g.jpg" alt ="icon1">
+							<p>
+								<b>
+								Lorem ipsum dolor sit amet, consectetur
+								<b>
+							</p>
+							<p>May 12,2019</p>
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pellentesque eros eu sollicitudin faucibus. Praesent vitae tortor auctor, tempus nisl ac, imperdiet ante. 
+							</p>
+						</div>
+						
+					</div>	
+					<div class="row">
+						<div class="col-twelve">
+							<div class="news-button">
+								<a href="#"> GO TO NEWS </a>
+							</div>
+						</div>
+					</div>	
+			</div>
+		</div>
+	</div>	
+	<div class="seen">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="seen-header">
+						<h2> AS SEEN IN</h2>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client1.png">	 
+					</div>
+				</div>
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client2.png">	 
+					</div>
+				</div>
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client3.png">	 
+					</div>
+				</div>
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client4.png">	 
+					</div>
+				</div>
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client5.png">	 
+					</div>
+				</div>
+				<div class="col-two">
+					<div class="seen-image">
+						<img src="images/client6.png">	 
+					</div>
+				</div>
+			</div>
+	
+			
+			<div class="row">
+				<div class="col-twelve">
+					<div class="seen-story">
+						<a href="#"> View customer stories > </a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-twelve">
+					<div class="seen-button">
+						<a href="#"> GO TO NEWS </a>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>
+	<div class="footer-data">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="footer-menu">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">Services</a></li>
+							<li><a href="#">News</a></li>
+							<li><a href="#">Pages</a></li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</div>	
+				</div>
+			</div>
+		</div>	
+	</div>
+		
+	<script src="all.min.js"></script>	
+</body>
+</html>
---
+++ webTemplate.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Web Template</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	
+	<div class="head">
+		<div class="container">
+			<div class="row">
+				<div class="col-two">
+					<div class="logo">
+						<img src ="icon/logo.png" alt="logo">
+					</div>
+				</div>	
+				<div class="col-ten">
+					<div class="top-menu"> 
+						
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">News</a></li>
+								<li><a href="#">Pages</a></li>
+								<li><a href="#">Contact</a></li>
+							</ul>
+						
+					</div>
+				</div>
+			</div>
+		</div>	
+    </div>
+	
+	<div class="section2">
+		<div class="container">
+			<div class="row clear-both">
+				<div class="col-four">
+					<div class="second-item1">
+						<h6>AWESOME</h6>
+						<h1>SERVICE SEO</h1>
+						<h1> TEMPLATE</h1>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+						</p>
+						<h4>Read More</h4>						
+						
+					</div>
+				</div>	
+				<div class="col-four">
+					<div class="second-item1">
+						<img src="icon/2nd/1.png" alt ="icon1">
+						<h3>Establish Goals</h3>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+						<h4>Read More</h4>
+					</div>	
+				</div>
+				<div class="col-four">
+					<div class="second-item1">
+						<img src="icon/2nd/3.png" alt ="icon1">
+						<h3>Establish Goals</h3>
+						<p>
+							imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+						</p>
+						<h4>Read More</h4>
+					</div>	
+				</div>
+			</div>			
+		
+		<div class="row clear-both">
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="second-item2">
+					<img src="icon/2nd/3.png" alt ="icon1">
+					<h3>Establish Goals</h3>
+					<p>
+						imply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+					</p>
+					<h4>Read More</h4>
+				</div>	
+			</div>
+		</div>	
+	</div>	
+</div>	
+<div class="web-solution">
+		<div class="container">
+			<div class="row">
+				<div class="col-three">
+					<div class ="web-image">
+						<img src="icon/about/plane.png" alt="web Image">
+					</div>
+				</div>
+				<div class="col-seven">
+					<div class="web-solution-data">
+						<h2>ALL-in-one Web Solution For</h2>
+						<h2>your business</h2>
+						<p>
+							simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to ma
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>	
+		
+		
+	<div class="our-work">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="our-work-header">
+						<h2> OUR WORK </h2>
+						<p> 
+							simply dummy text of the printing and typesetting industry,<br> Lorem Ipsum has been the industry's s
+						</p>
+					</div>
+				</div>
+			</div>	
+			<div class="row">
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>
+				</div>	
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>	
+				</div>
+				<div class="col-four">
+					<div class="work-item1">
+						<img src="images/g.jpg" alt ="icon1">
+					</div>	
+				</div>
+			</div>			
+		
+		<div class="row">
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+			<div class="col-four">
+				<div class="work-item2">
+					<img src="images/g.jpg" alt ="icon1">
+				</div>	
+			</div>
+
+		</div>
+		<div class="row">
+			<div class="col-twelve">
+				<div class="work-button">
+					<h3> VIEW CAR WORK</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
+	
+	<div class="our-testimony">
+		<div class="container">
+			<div class="row">
+				<div class="col-twelve">
+					<div class="testimony-item">
+						<h1> Our Testimony</h1>
+						<img src="icon/testimony/1.png" alt ="testimony icon">
+					</div>	
+				</div>
+			</div>	
+		</div>
+	</div>
+	
+	
+		
+</body>
+</html>
